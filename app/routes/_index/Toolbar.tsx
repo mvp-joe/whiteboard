f98a5214711af6083589682b045aab2f69e8398d
Joseph Ward <joe@studiomvp.com>
2024-10-30 07:13:59 -0600

    select new rectangle on add

--- app/routes/_index/Toolbar.tsx
+++ app/routes/_index/Toolbar.tsx
@@ -2,41 +2,45 @@ import { useCanRedo, useCanUndo, useMutation, useRedo, useUndo } from "@livebloc
 import { ActionIcon, Divider, Flex, Group, Paper, Tooltip } from "@mantine/core";
 import { ArrowClockwise, ArrowCounterClockwise, Eraser, Rectangle } from "@phosphor-icons/react";
 import { v4 as uuid } from 'uuid';
+import { useWhiteboardStore } from "~/routes/_index/store";
 
 export function Toolbar() {
     const canUndo = useCanUndo()
     const undo = useUndo()
     const canRedo = useCanRedo()
     const redo = useRedo()
-  
+    const select = useWhiteboardStore(s => s.select)
+
     const eraseAll = useMutation((c) => {
-      c.storage.get('shapes').clear()    
+        c.storage.get('shapes').clear()
     }, [])
-  
+
     const addRectangle = useMutation((c) => {
-      c.storage.get('shapes').push({ id: uuid(), type: 'rectangle', top: 20, left: 20, width: 100, height: 100 })
+        const id = uuid()
+        c.storage.get('shapes').push({ id: id, type: 'rectangle', top: 20, left: 20, width: 100, height: 100 })
+        select(id, false)
     }, [])
-  
+
     return (
-      <Flex align="center" justify="center">
-        <Paper withBorder radius="md" shadow="md">
-          <Group p={6} gap="xs" >
-            <Tooltip label="Add Rectangle">
-              <ActionIcon variant="transparent" size="lg" onClick={addRectangle}><Rectangle size={20} /></ActionIcon>
-            </Tooltip>          
-            <Divider orientation="vertical" />          
-            <Tooltip label="Erase All">
-              <ActionIcon variant="transparent" size="lg" onClick={eraseAll}><Eraser size={22} /></ActionIcon>
-            </Tooltip>
-            <Divider orientation="vertical" />          
-            <Tooltip label="Undo">
-              <ActionIcon variant="transparent" size="lg" disabled={!canUndo} ><ArrowCounterClockwise size={20} onClick={undo} /> </ActionIcon>
-            </Tooltip>
-            <Tooltip label="Redo">
-              <ActionIcon variant="transparent" size="lg" disabled={!canRedo} ><ArrowClockwise size={20} onClick={redo} /> </ActionIcon>
-            </Tooltip>
-          </Group>
-        </Paper>
-      </Flex>
+        <Flex align="center" justify="center">
+            <Paper withBorder radius="md" shadow="md">
+                <Group p={6} gap="xs" >
+                    <Tooltip label="Add Rectangle">
+                        <ActionIcon variant="transparent" size="lg" onClick={addRectangle}><Rectangle size={20} /></ActionIcon>
+                    </Tooltip>
+                    <Divider orientation="vertical" />
+                    <Tooltip label="Erase All">
+                        <ActionIcon variant="transparent" size="lg" onClick={eraseAll}><Eraser size={22} /></ActionIcon>
+                    </Tooltip>
+                    <Divider orientation="vertical" />
+                    <Tooltip label="Undo">
+                        <ActionIcon variant="transparent" size="lg" disabled={!canUndo} ><ArrowCounterClockwise size={20} onClick={undo} /> </ActionIcon>
+                    </Tooltip>
+                    <Tooltip label="Redo">
+                        <ActionIcon variant="transparent" size="lg" disabled={!canRedo} ><ArrowClockwise size={20} onClick={redo} /> </ActionIcon>
+                    </Tooltip>
+                </Group>
+            </Paper>
+        </Flex>
     )
-  }
+}
